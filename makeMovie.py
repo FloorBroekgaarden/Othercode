083eeff52d4611439acaebbda92335551c70bc07
FloorBroekgaarden <floor.broekgaarden@cfa.harvard.edu>
2023-03-30 14:55:26 -0400

    update plots

--- makeMovie.py
+++ makeMovie.py
@@ -14,7 +14,7 @@ import glob
 
 
 
-def makeMovie_GWcatalogSize(fps=20, duration=60, name_images='none'):
+def makeMovie_GWcatalogSize(fps=20, duration=60, name_images='none', number_images=400):
 	'''
 	whichRate = 'intrinsic' or 'observed'
 	fps=0.4, frames per second
@@ -25,7 +25,49 @@ def makeMovie_GWcatalogSize(fps=20, duration=60, name_images='none'):
 
 	images = []
 
-	for ind_im  in range(200):
+	for ind_im  in range(number_images):
+			images.append(image_folder +   name_images  + str(ind_im) + '.png')
+
+
+
+	image_files = images
+	clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(image_files, fps=fps)
+	clip.write_videofile(image_folder+'movie_'+ name_images  + '.mp4')
+
+
+	# make also gif:
+ 
+	# Create the frames
+	frames = []
+	# imgs = glob.glob("*.png")
+	for i in images:
+	    new_frame = Image.open(i)
+	    frames.append(new_frame)
+	 
+	# Save into a GIF file that loops forever
+	frames[0].save(image_folder+'gif_'+ name_images +  '.gif', format='GIF',
+	               append_images=frames[1:],
+	               save_all=True,
+	               duration=duration, loop=0)
+
+
+	print('done')
+	return 
+
+
+
+def makeMovie_GWcatalogSizeJustConfirmed(fps=20, duration=60, name_images='none'):
+	'''
+	whichRate = 'intrinsic' or 'observed'
+	fps=0.4, frames per second
+	duration = duration of the movie 
+	'''
+
+	image_folder = '/Users/floorbroekgaarden/Projects/GitHub/Othercode/GWcatalogFigures/'
+
+	images = []
+
+	for ind_im  in range(4):
 			images.append(image_folder +   name_images  + str(ind_im) + '.png')
 
 
@@ -155,11 +197,14 @@ Movie_GWcatalogSize=True
 # Run this using python 3!! 
 
 if Movie_GWcatalogSize==True:
-	makeMovie_GWcatalogSize(fps=20, duration=60, name_images='GWcatalogSize_UncertaintyLin_')
-	makeMovie_GWcatalogSize(fps=20, duration=60, name_images='GWcatalogSize_withUpdatedCE_')
-	makeMovie_GWcatalogSize(fps=20, duration=60, name_images='GWcatalogSize_withUncertainty_')
 
-	# makeMovie_GWcatalogSizeO4()
+	# makeMovie_GWcatalogSizeJustConfirmed(fps=1.2, duration=200, name_images='knownDetectionsGWcatalogSize_')
+	# # makeMovie_GWcatalogSize(fps=45, duration=40, name_images='knownDetectionsGWcatalogSizeMovie_') # make movie different O1-O3 catalogs 
+	# makeMovie_GWcatalogSize(fps=20, duration=60, name_images='GWcatalogSize_UncertaintyLin_', number_images=200)
+	# makeMovie_GWcatalogSize(fps=20, duration=60, name_images='GWcatalogSize_withUpdatedCE_', number_images=200)
+	# makeMovie_GWcatalogSize(fps=20, duration=60, name_images='GWcatalogSize_withUncertainty_', number_images=200)
+
+	makeMovie_GWcatalogSizeO4()
 	# makeMovie_GWcatalogSize()
 	# makeMovie_GWcatalogSize_log()
 
